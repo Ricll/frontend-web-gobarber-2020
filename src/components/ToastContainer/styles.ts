079e0f5f043ea13692d617ee875282a8d0dd9c43
Ricll <ricllima@hotmail.com>
2020-10-06 00:28:05 -0300

    first commit

---
+++ src/components/ToastContainer/styles.ts
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import styled from 'styled-components';
+
+// eslint-disable-next-line no-unused-vars
+
+export const Container = styled.div`
+  position: absolute;
+  right: 0;
+  top: 0%;
+  padding: 30px;
+  overflow: hidden;
+`;
+
+// eslint-disable-next-line no-undef
